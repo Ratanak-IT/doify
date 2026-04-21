75f78586d42fc04f6a530f534fd99a63f736946a
ratanak <ratanak1intel@gmail.com>
2026-04-21 20:37:04 +0700

    navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -75,7 +75,7 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="sticky top-0 z-100">
+    <header className="fixed top-0 z-100 w-full">
       <nav className=" transition-all duration-300 bg-white/95 dark:bg-slate-900/95 backdrop-blur-md border-b border-slate-200/20 dark:border-slate-700/50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between gap-5">
           {/* Logo */}
